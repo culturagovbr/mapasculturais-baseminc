c1f5d995f11fd7edb054eac605969cbb7d4a7a0a
Gustavo Lima da Silva <gustavo.silva.vs@gmail.com>
2017-03-02 16:16:50 -0300

    Fix filter when theme is not extending Subsite

--- plugins/FilterSubsite/Plugin.php
+++ plugins/FilterSubsite/Plugin.php
@@ -15,29 +15,31 @@ class Plugin extends \MapasCulturais\Plugin {
 
         $subsite_id = $app->getCurrentSubsiteId();
 
-        $app->hook("API.<<*>>(project).params", function(&$params) use($subsite_id){
-            $subsite = \MapasCulturais\App::i()->repo('Subsite')->find($subsite_id);
-            if ($subsite->show_instance_only_project == 's')
-                $params['_subsiteId'] = "EQ($subsite_id)";
-        });
+        if ($subsite_id) {
+            $app->hook("API.<<*>>(project).params", function(&$params) use($subsite_id){
+                $subsite = \MapasCulturais\App::i()->repo('Subsite')->find($subsite_id);
+                if ($subsite->show_instance_only_project == 's')
+                    $params['_subsiteId'] = "EQ($subsite_id)";
+            });
 
-        $app->hook("API.<<*>>(space).params", function(&$params) use($subsite_id){
-            $subsite = \MapasCulturais\App::i()->repo('Subsite')->find($subsite_id);
-            if ($subsite->show_instance_only_space == 's')
-                $params['_subsiteId'] = "EQ($subsite_id)";
-        });
+            $app->hook("API.<<*>>(space).params", function(&$params) use($subsite_id){
+                $subsite = \MapasCulturais\App::i()->repo('Subsite')->find($subsite_id);
+                if ($subsite->show_instance_only_space == 's')
+                    $params['_subsiteId'] = "EQ($subsite_id)";
+            });
 
-        $app->hook("API.<<*>>(agent).params", function(&$params) use($subsite_id){
-            $subsite = \MapasCulturais\App::i()->repo('Subsite')->find($subsite_id);
-            if ($subsite->show_instance_only_agent == 's')
-                $params['_subsiteId'] = "EQ($subsite_id)";
-        });
+            $app->hook("API.<<*>>(agent).params", function(&$params) use($subsite_id){
+                $subsite = \MapasCulturais\App::i()->repo('Subsite')->find($subsite_id);
+                if ($subsite->show_instance_only_agent == 's')
+                    $params['_subsiteId'] = "EQ($subsite_id)";
+            });
 
-        $app->hook("API.<<*>>(event).params", function(&$params) use($subsite_id){
-            $subsite = \MapasCulturais\App::i()->repo('Subsite')->find($subsite_id);
-            if ($subsite->show_instance_only_event == 's')
-                $params['_subsiteId'] = "EQ($subsite_id)";
-        });
+            $app->hook("API.<<*>>(event).params", function(&$params) use($subsite_id){
+                $subsite = \MapasCulturais\App::i()->repo('Subsite')->find($subsite_id);
+                if ($subsite->show_instance_only_event == 's')
+                    $params['_subsiteId'] = "EQ($subsite_id)";
+            });
+        }
     }
 
     public function register() {
