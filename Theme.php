6ae5c54388c24cfd7f52475d338e3ca3626c55b4
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2019-02-27 14:51:53 -0300

    Adds RCV`s specific hook

--- Theme.php
+++ Theme.php
@@ -257,6 +257,27 @@ class Theme extends Subsite\Theme{
                 $params["@select"] = str_replace(",isVerified","",$params["@select"]);
             }
         });
+
+        $app->hook('can-edit', function(&$can_edit, $entity) use ($app) {
+            $user_id = $entity->user->id;
+
+            $em = $app->em;
+            $conn = $em->getConnection();
+            $id = $conn->fetchAssoc("SELECT profile_id FROM usr where id = ".$user_id);
+            if(!empty($id))
+            {
+                $id = $id['profile_id'];
+                $result = $conn->fetchAssoc("SELECT * FROM agent_meta WHERE key = 'rcv_tipo' and (value = 'entidade' OR value = 'ponto' OR value = 'responsavel')  AND object_id ='$id'");
+            }
+
+            if(empty($result))
+            {
+                $can_edit = true;
+            }
+            else {
+                $can_edit = false;
+            }
+        });
     }
 
     public function includeAngularEntityAssets($entity) {
